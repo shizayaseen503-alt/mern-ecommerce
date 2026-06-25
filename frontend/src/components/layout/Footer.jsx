56d52490c2ac3a116b2af33f4124bb0552f67260
shizayaseen503-alt <shizayaseen503@gmail.com>
2026-06-25 23:45:07 +0500

    Update Footer.jsx

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 import { FaTwitter, FaInstagram, FaFacebookF, FaPinterestP } from "react-icons/fa";
 
 export default function Footer() {
