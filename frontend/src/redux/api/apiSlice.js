3a2cb0b9eada9c0fa2a9f7486064ae98fe80fb4c
shizayaseen503-alt <shizayaseen503@gmail.com>
2026-06-25 23:06:09 +0500

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,9 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      import.meta.env.VITE_API_URL ||
-      "https://mern-ecommerce-production-4dac.up.railway.app/api",
+    baseUrl: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
     credentials: "include",
   }),
   tagTypes: ["Product", "Order", "User", "Category", "Cart", "Favorites"],
