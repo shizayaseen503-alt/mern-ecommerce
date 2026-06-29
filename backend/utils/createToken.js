709a28a6afe0091dddb90b6b9dc0b9b7aba23b40
shizayaseen503-alt <shizayaseen503@gmail.com>
2026-06-29 16:16:47 +0500

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,9 +7,10 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,     
+    sameSite: "none",  
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
+
 export { generateToken };
